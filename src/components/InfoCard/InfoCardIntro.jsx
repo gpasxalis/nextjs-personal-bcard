78ee6b35c8af4b3624616df90a58daede5f17dea
gpasxalis <gpasxalis94@gmail.com>
2024-12-31 17:10:43 +0200

    Fixes

--- src/components/InfoCard/InfoCardIntro.jsx
+++ src/components/InfoCard/InfoCardIntro.jsx
@@ -6,12 +6,16 @@ const InfoCardIntro = (props) => {
 
             <h2 className="font-medium text-dark-gray text-base text-center -mt-4">{props.subtitle}</h2>
 
-            <div className="flex flex-col w-full gap-2" >
+            <div className="flex flex-col w-full gap-2 mt-2" >
+                
                 <p className="font-firaSans font-normal text-dark-gray text-base">💻 Μετατρέποντας ιδέες σε ψηφιακές λύσεις</p>
 
-                <p className="font-firaSans font-normal text-dark-gray text-base">💡 Dalution Project</p>
+                <p className="font-firaSans font-normal text-dark-gray text-base">🚀 Building PeachLabs</p>
+
+                <p className="font-firaSans font-normal text-dark-gray text-base">💡 Dalution Websites Project</p>
 
                 <p className="font-firaSans font-normal text-dark-gray text-base">📍 Greece, Alexandroupolis (AXD)</p>
+
             </div>
 
             
