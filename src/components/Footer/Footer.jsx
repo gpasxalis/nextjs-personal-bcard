95cca6110c72427a58c2b608fd6cd84f6685d5c6
gpasxalis <gpasxalis94@gmail.com>
2025-01-03 14:40:29 +0200

    Project paragraphs

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Footer = () => {
     return(
         <div className="bg-yellow w-full rounded-md h-min p-2 mt-2 text-dark-blue">
-            <Link href="https://dalution.gr" target="_blank" className="flex justify-center items-center font-medium">
+            <Link href="https://dalution.gr" target="_blank" className="flex justify-center items-center font-medium text-base">
                 Powered by Dalution
             </Link>
         </div>
