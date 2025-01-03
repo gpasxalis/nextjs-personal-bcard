620d88a4b563f9acb8a7b6835d7729b55e578db4
gpasxalis <gpasxalis94@gmail.com>
2025-01-03 14:36:08 +0200

    General Fixes

--- src/components/InfoCard/InfoCardItem.jsx
+++ src/components/InfoCard/InfoCardItem.jsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 
 const InfoCardItem = (props) => {
     return(
-        <div className="bg-dark-blue w-full h-16 flex items-center py-3 px-1 rounded-md border-2 text-off-white gap-10">
+        <div className="bg-dark-blue w-full h-16 flex items-center py-3 px-1 rounded-md border-2 text-off-white gap-10 shadow-sm">
             <div className="h-12 w-20 flex p-3 justify-center items-center border-r-2 border-off-white">
                 <Link href={props.link} target="_blank">
                     <ContainerIcons icon={props.icon}/>
                 </Link>
             </div>
-            <h3 className="font-normal font-geistSans">
+            <h3 className="font-normal font-geistSans text-base">
                 <Link href={props.link} target="_blank">
                     {props.text}
                 </Link>
