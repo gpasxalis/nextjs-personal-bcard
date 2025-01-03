7d9db5bbc16b5494ce16376dcba73c04f0a8f33a
gpasxalis <gpasxalis94@gmail.com>
2025-01-03 11:50:59 +0200

    Web Share Fixes

--- src/components/Misc/ShareButton.jsx
+++ src/components/Misc/ShareButton.jsx
@@ -1,19 +1,24 @@
-'use client'
+'use client';
 
 import { RWebShare } from "react-web-share";
 import { BsShare } from "react-icons/bs";
+import { usePathname } from "next/navigation";
 
-const shareData = {
-    title: "Test Title",
-    text: "Test text",
-    url: "https://dalution.gr",
-}
+
 
 const ShareButton = () => {
+    const router = usePathname();
+
+    const shareData = {
+        title: "Test Title",
+        text: "Test text",
+        url: router,
+    }
+
     return(
 
         <RWebShare data={shareData} >
-            <button className="py-3 px-4 text-off-white">
+            <button className="py-3 px-4 text-off-white text-base">
                 <BsShare />
             </button>
         </RWebShare>
