81ffa7f5b2e3e3956d5cb2a39fc498e2822ab4b1
gpasxalis <gpasxalis94@gmail.com>
2025-01-03 17:18:56 +0200

    Action Buttons Fixes

--- src/components/InfoCard/ActionButtons.jsx
+++ src/components/InfoCard/ActionButtons.jsx
@@ -1,22 +1,23 @@
 import { FiPhoneCall } from "react-icons/fi";
 import { BsEnvelope } from "react-icons/bs";
-import { IoIosContact } from "react-icons/io";
+import { BsPersonVcard } from "react-icons/bs";
+import Link from "next/link";
 
 const ActionButtons = () => {
 
     return(
         <div className="flex w-full justify-center gap-5">
-            <button className="p-3 bg-yellow text-xl text-dark-blue rounded-full shadow-sm" title="Call Me">
+            <Link className="p-3 bg-yellow text-xl text-dark-blue rounded-full shadow-sm" title="Call Me" href="tel:+306978101030">
                 <FiPhoneCall />
-            </button>
+            </Link>
 
-            <button className="p-3 bg-yellow text-xl text-dark-blue rounded-full shadow-sm" title="E-mail Me">
+            <Link className="p-3 bg-yellow text-xl text-dark-blue rounded-full shadow-sm" title="E-mail Me" href="mailto:paschalis@dalution.gr">
                 <BsEnvelope />
-            </button>
+            </Link>
 
-            <button className="p-3 bg-yellow text-xl text-dark-blue rounded-full shadow-sm" title="Add me to your Contacts">
-                <IoIosContact />
-            </button>
+            <Link className="p-3 bg-yellow text-xl text-dark-blue rounded-full shadow-sm" title="Add me to your Contacts" href="/vcard.vcf" target="_blank" rel="noreferrer" download>
+                <BsPersonVcard />
+            </Link>
         </div>
     );
 };
