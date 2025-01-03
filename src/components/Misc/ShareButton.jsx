63a8c7032905e9570e68a576ce3ef77ef51e6b61
gpasxalis <gpasxalis94@gmail.com>
2025-01-03 11:52:18 +0200

    Web Share Fixes

--- src/components/Misc/ShareButton.jsx
+++ src/components/Misc/ShareButton.jsx
@@ -18,7 +18,7 @@ const ShareButton = () => {
     return(
 
         <RWebShare data={shareData} >
-            <button className="py-3 px-4 text-off-white text-base">
+            <button className="py-3 px-4 text-off-white text-lg">
                 <BsShare />
             </button>
         </RWebShare>
