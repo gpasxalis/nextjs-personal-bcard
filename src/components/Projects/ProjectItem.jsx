db0bb02b54516f4bedd3de9c0411f5e71da2da6f
gpasxalis <gpasxalis94@gmail.com>
2025-01-03 14:30:10 +0200

    Projects Edits

--- src/components/Projects/ProjectItem.jsx
+++ src/components/Projects/ProjectItem.jsx
@@ -10,16 +10,16 @@ const ProjectItem = (props) => {
         <div className="bg-dark-blue rounded-xl">
             <Image alt={props.title} src={props.image} sizes="100%" className="rounded-t-xl" />
             <div className="flex flex-col px-3 py-3.5 gap-3.5">
-                <h3 className="font-extrabold text-yellow text-lg uppercase">
+                <h3 className="font-extrabold text-yellow text-base uppercase">
                     {props.title}
                 </h3>
-                <p className="text-peachybeige">
+                <p className="text-off-white text-sm">
                     {props.paragraph}
                 </p>
                 <div className="w-36 bg-transparent border-2 border-yellow py-1 rounded-full self-center hover:bg-yellow text-yellow hover:text-dark-gray">
                     <Link href={props.link} target="_blank" className="flex items-center justify-center gap-2 uppercase">
                         <FiLink fontSize={16}/>
-                        <h4 className="text-md font-semibold">Check it</h4>
+                        <h4 className="text-md font-semibold text-sm">Check it</h4>
                     </Link>
                 </div>
             </div>
